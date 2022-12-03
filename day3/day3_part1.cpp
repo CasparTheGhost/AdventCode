6a55b8892dbd2dfe1320fbd24a78cbebbf7e480d
theRandString <caspar.qst1@gmail.com>
2022-12-03 13:26:11 +0100

    Added Code comments

--- day3/day3_part1.cpp
+++ day3/day3_part1.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 
 
+//compare each char in both strings and get the corresponding prio
 int compare(std::unordered_map<int, int> *ascii, char arr[2][30], int len) {
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len + 1; j++) {
@@ -29,6 +30,8 @@ int main() {
         return 0;
     }
 
+    // loop through ascii table and assign corresponding priorities to the chars
+    //first loop starts from small a and goes until z and the second loop starts with the capital A to Z
     int prio_abc = 1, prio_ABC = 27;
     for (int i = 97; i < 123; i++) {
         ascii.insert(std::make_pair(i, prio_abc));
@@ -42,8 +45,11 @@ int main() {
     //reads until EOF
     while (!feof(file)) {
         fscanf(file, "%s\n", &chunk);
+        //Get half of line len
         int len = strlen(chunk) / 2;
+        //copy first and second half of line into a buffer
         memcpy(split_chunk[0], chunk, len);
+        //terminating the string
         split_chunk[0][len] = '\0';
         memcpy(split_chunk[1], chunk + len, len + 1);
         split_chunk[1][len] = '\0';
